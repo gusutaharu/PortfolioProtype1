3ab23840e23572911c7a872af21a0e0a09e89815
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-04-08 09:02:14 +0900

    feat: motionライブラリを用いてヘッダーにアニメーションを実装

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AnimatePresence, motion } from 'motion/react';
 import Link from 'next/link';
 import { useState } from 'react';
 
@@ -12,6 +13,20 @@ const navItems = [
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const variantTop = {
+    closed: { rotate: 0, y: 0 },
+    opened: { rotate: 45, y: 9 },
+  };
+
+  const variantCenter = {
+    closed: { opacity: 1, x: 0 },
+    opened: { opacity: 0, x: 20 },
+  };
+
+  const variantBottom = {
+    closed: { rotate: 0, y: 0 },
+    opened: { rotate: -45, y: -9 },
+  };
 
   return (
     <header>
@@ -29,22 +44,47 @@ export const Header = () => {
           </ul>
         </nav>
         <button className="hamburger-btn" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? 'X' : 'Menu'}
+          <motion.span
+            variants={variantTop}
+            animate={isOpen ? 'opened' : 'closed'}
+            transition={{ type: 'spring', stiffness: 260, damping: 20 }}
+            className="btn-bar"
+          />
+          <motion.span
+            variants={variantCenter}
+            animate={isOpen ? 'opened' : 'closed'}
+            transition={{ duration: 0.2 }}
+            className="btn-bar"
+          />
+          <motion.span
+            variants={variantBottom}
+            animate={isOpen ? 'opened' : 'closed'}
+            transition={{ type: 'spring', stiffness: 260, damping: 20 }}
+            className="btn-bar"
+          />
         </button>
       </div>
-      {isOpen && (
-        <nav className="nav-mobile">
-          <ul>
-            {navItems.map((item) => (
-              <li key={item.name}>
-                <Link href={item.href} onClick={() => setIsOpen(false)}>
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.nav
+            className="nav-mobile"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
+          >
+            <ul>
+              {navItems.map((item) => (
+                <li key={item.name}>
+                  <Link href={item.href} onClick={() => setIsOpen(false)}>
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </motion.nav>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
