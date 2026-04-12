f4171acb8d95ad578e09eb27bf595aa7ad80cad1
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-04-12 13:32:51 +0900

    style: ディレクトリ名の変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Hero } from '@/components/sections/hero';
-import { Skills } from '@/components/sections/skils';
+import { Skills } from '@/components/sections/skills';
 
 export default function Home() {
   return (
