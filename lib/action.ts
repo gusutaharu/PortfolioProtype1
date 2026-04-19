94baa740492d1fee45c8b054f63434e5dd612d9a
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-04-19 23:59:14 +0900

    feat: zodによる検証を実装

--- lib/action.ts
+++ lib/action.ts
@@ -1,12 +1,38 @@
 'use server';
 
-export const sendEamil = async (formData: FormData) => {
-  const name = formData.get('name') as string;
-  const email = formData.get('email') as string;
-  const content = formData.get('content') as string;
+import { z } from 'zod';
+
+const ContactSchema = z.object({
+  name: z
+    .string()
+    .min(2, { error: '名前は二文字以上入力してください' })
+    .max(50, { error: '名前は五十文字以内に収めてください' }),
+  email: z.email({ error: '正しいメールアドレスをご入力ください' }),
+  content: z
+    .string()
+    .min(5, { error: '内容は５文字以上入力してください' })
+    .max(500, { error: '内容は500文字以内に収めてください' }),
+});
+
+export const sendEmail = async (formData: FormData) => {
+  const validatedFields = ContactSchema.safeParse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    content: formData.get('content'),
+  });
+  if (!validatedFields.success) {
+    return {
+      success: false,
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+  const { name, email, content } = validatedFields.data;
   try {
     console.log(`${name},${email},${content}`);
   } catch (error) {
-    return console.log(`失敗しました${error}`);
+    return {
+      success: false,
+      message: '送信に失敗しました。時間をおいて再度お試しください。',
+    };
   }
 };
