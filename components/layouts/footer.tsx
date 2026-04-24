ddbf1bebaeef8d637b272fea2cb93d04d1d21937
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-04-24 23:40:07 +0900

    feat:footerの仮設置

---
+++ components/layouts/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <span>
+        &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
+      </span>
+    </footer>
+  );
+};
