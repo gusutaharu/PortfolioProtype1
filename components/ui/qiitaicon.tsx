2972e81e20633994a6324de48f0a844f261dfba2
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-05-12 11:15:11 +0900

    fix: qiitaiconファイル周りの修正

--- components/ui/qiitaicon.tsx
+++ components/ui/qiitaicon.tsx
@@ -3,7 +3,11 @@ import { SiQiita } from 'react-icons/si';
 
 export const QiitaIcon = () => {
   return (
-    <Link href={'/'} target="_blank" rel="noopener noreferrer">
+    <Link
+      href={'https://qiita.com/gusutaharu'}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <SiQiita size={40} className="link-icon" />
     </Link>
   );
